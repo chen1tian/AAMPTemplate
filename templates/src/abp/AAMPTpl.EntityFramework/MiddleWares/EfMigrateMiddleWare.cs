97260a7c8a242b16d51766d731d44be228c7b126
陈艺天 <48295852@qq.com>
2026-01-07 11:16:05 +0800

    增加数据库自动迁移中间件代码

---
+++ templates/src/abp/AAMPTpl.EntityFramework/MiddleWares/EfMigrateMiddleWare.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AAMPTpl.EntityFramework.MiddleWares
+{
+    public static class EfMigrateMiddleWare
+    {
+        /// <summary>
+        /// 自动合并数据库
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="db"></param>
+        public static void UseAutoMigration(this IServiceCollection services, DbContext db)
+        {
+            MigrationDb(db);
+        }
+
+        /// <summary>
+        /// 自动合并数据库
+        /// </summary>
+        /// <typeparam name="TDbContext"></typeparam>
+        /// <param name="app"></param>
+        public static void UseAutoMigration<TDbContext>(this IServiceCollection services)
+            where TDbContext : DbContext
+        {
+            using (var serviceScope = services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var dbContext = serviceScope.ServiceProvider.GetRequiredService<TDbContext>();
+                services.UseAutoMigration(dbContext);
+            }
+        }
+
+        public static void MigrationDb(DbContext db)
+        {
+            // 自动迁移数据库 
+            if (db.Database.GetPendingMigrations().Any())
+            {
+                Console.WriteLine($"{db.GetType().Name} Migrating...");
+
+                db.Database.Migrate();
+
+                Console.WriteLine($"{db.GetType().Name} Database Migrated");
+            }
+            else
+            {
+                Console.WriteLine($"{db.GetType().Name} No Need Migrate");
+            }
+
+            Console.WriteLine($"{db.GetType().Name} Check Migrations Complete!");
+            Console.WriteLine("");
+        }
+    }
+}
