4e4bfcc5f3f08d41e25a1127a001aed65a555355
陈艺天 <48295852@qq.com>
2026-07-02 15:24:29 +0800

    使用avalonia原生导航系统

--- templates/src/AAMPTpl/ViewModels/MainViewModel.cs
+++ templates/src/AAMPTpl/ViewModels/MainViewModel.cs
@@ -1,6 +1,4 @@
-﻿using Azure;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 
 namespace AAMPTpl.ViewModels;
@@ -27,36 +25,32 @@ public partial class MainViewModel : ViewModelBase
     private NavMenuItem _selectedMenuItem;
 
     [ObservableProperty]
-    private bool _navDrawerSwitch;
+    private bool _navDrawerSwitch = true;
 
-    [ObservableProperty]
-    private bool _isMenuCollapsed;
-
-    partial void OnIsMenuCollapsedChanged(bool value)
-    {        
-        OnPropertyChanged(nameof(MenuWidth));
+    partial void OnNavDrawerSwitchChanged(bool value)
+    {
         OnPropertyChanged(nameof(IsMenuTextVisible));
         OnPropertyChanged(nameof(ToggleMenuIcon));
     }
 
-    public double MenuWidth => IsMenuCollapsed ? 56 : 120;
-    public bool IsMenuTextVisible => !IsMenuCollapsed;
-    public string ToggleMenuIcon => IsMenuCollapsed
-        ? "M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"
-        : "M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z";
+    public bool IsMenuTextVisible => NavDrawerSwitch;
 
-    [RelayCommand]
-    private void ToggleMenu() => IsMenuCollapsed = !IsMenuCollapsed;
+    public string ToggleMenuIcon => NavDrawerSwitch
+        ? "M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"
+        : "M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z";
+
+    public string CurrentPageTitle => SelectedMenuItem.Title;
 
     public ObservableCollection<NavMenuItem> MenuItems { get; } =
     [
         new NavMenuItem { Title = "菜单1", Icon = "M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4M12,6A6,6 0 0,0 6,12A6,6 0 0,0 12,18A6,6 0 0,0 18,12A6,6 0 0,0 12,6M12,8A4,4 0 0,1 16,12A4,4 0 0,1 12,16A4,4 0 0,1 8,12A4,4 0 0,1 12,8Z" },
-        new NavMenuItem { Title = "菜单2", Icon = "M22,21H2V3H4V19H6V10H10V19H12V6H16V19H18V14H22V21Z" },        
+        new NavMenuItem { Title = "菜单2", Icon = "M22,21H2V3H4V19H6V10H10V19H12V6H16V19H18V14H22V21Z" },
     ];
 
     partial void OnSelectedMenuItemChanged(NavMenuItem value)
     {
         SwitchPage(value);
+        OnPropertyChanged(nameof(CurrentPageTitle));
     }
 
     private void SwitchPage(NavMenuItem menuItem)
@@ -64,7 +58,7 @@ public partial class MainViewModel : ViewModelBase
         CurrentPage = menuItem.Title switch
         {
             "菜单1" => _page1,
-            "菜单2" => _page2,        
+            "菜单2" => _page2,
             _ => _page1
         };
     }
--- templates/src/AAMPTpl/Views/MainView.axaml.cs
+++ templates/src/AAMPTpl/Views/MainView.axaml.cs
@@ -1,11 +1,83 @@
 using Avalonia.Controls;
+using AAMPTpl.ViewModels;
+using System.ComponentModel;
+using System.Threading.Tasks;
 
 namespace AAMPTpl.Views;
 
 public partial class MainView : UserControl
 {
+    private MainViewModel? _viewModel;
+    private ViewModelBase? _navigatedPage;
+
     public MainView()
     {
         InitializeComponent();
+        DataContextChanged += OnDataContextChanged;
+    }
+
+    private void OnDataContextChanged(object? sender, System.EventArgs e)
+    {
+        if (_viewModel is not null)
+        {
+            _viewModel.PropertyChanged -= OnViewModelPropertyChanged;
+        }
+
+        _viewModel = DataContext as MainViewModel;
+
+        if (_viewModel is null)
+        {
+            MainNavigation.Content = null;
+            _navigatedPage = null;
+            return;
+        }
+
+        _viewModel.PropertyChanged += OnViewModelPropertyChanged;
+        NavigateToCurrentPage(replaceCurrentPage: false);
+    }
+
+    private async void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(MainViewModel.CurrentPage))
+        {
+            await NavigateToCurrentPageAsync(replaceCurrentPage: true);
+        }
+    }
+
+    private void NavigateToCurrentPage(bool replaceCurrentPage)
+    {
+        _ = NavigateToCurrentPageAsync(replaceCurrentPage);
+    }
+
+    private async Task NavigateToCurrentPageAsync(bool replaceCurrentPage)
+    {
+        if (_viewModel is null || ReferenceEquals(_navigatedPage, _viewModel.CurrentPage))
+        {
+            return;
+        }
+
+        var page = CreateContentPage(_viewModel.CurrentPage, _viewModel.CurrentPageTitle);
+
+        if (!replaceCurrentPage || MainNavigation.StackDepth == 0)
+        {
+            MainNavigation.Content = page;
+        }
+        else
+        {
+            await MainNavigation.ReplaceAsync(page);
+        }
+
+        _navigatedPage = _viewModel.CurrentPage;
+    }
+
+    private static ContentPage CreateContentPage(ViewModelBase pageViewModel, string header)
+    {
+        return new ContentPage
+        {
+            Header = header,
+            Content = pageViewModel,
+            HorizontalContentAlignment = Avalonia.Layout.HorizontalAlignment.Stretch,
+            VerticalContentAlignment = Avalonia.Layout.VerticalAlignment.Stretch
+        };
     }
 }
